e6b9be6a2249e20322ef0e18bb88f9bd8af31fc5
Fiifi <felixarthur264@gmail.com>
2022-08-18 10:08:07 +0000

    Added cart page and changes in other pages

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+// Forms
 function validate() {
     document.getElementById('form').classList.add('was-validated');
+}
+
+// Navbar
+const nav = document.getElementById('navbar');
+document.onscroll = function() {
+    if(nav) {
+        nav.style.transition = '.2s';
+        if(scrollY > 3) {
+            makeDark();
+        }
+        else {
+            makeTransparent();
+        }
+    }
+}
+
+function makeDark() {
+    nav.classList.add('bg-black');
+}
+
+function makeTransparent() {
+    nav.classList.remove('bg-black');
 }
